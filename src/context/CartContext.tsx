aae563fba1b12b91414dcb55d32d383636f68fb1
VitorPerraro <vitor.perraro@outlook.com>
2024-09-25 17:30:05 -0300

    fix: correção de bug vercel

--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { createContext, useContext, useState, ReactNode } from 'react';
+
 interface CartItem {
   id: number;
   title: string;
@@ -10,11 +11,13 @@ interface CartItem {
 
 interface CartContextType {
   cart: CartItem[];
-  addToCart: (item: CartItem) => void;
+  addToCart: (item: CartItem) => void; 
   removeFromCart: (id: number) => void;
 }
 
+
 const CartContext = createContext<CartContextType | undefined>(undefined);
+
 
 export const useCart = () => {
   const context = useContext(CartContext);
@@ -26,24 +29,25 @@ export const useCart = () => {
 
 
 export const CartProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
-
   const [cart, setCart] = useState<CartItem[]>([]);
 
   const addToCart = (item: CartItem) => {
-
     setCart((prevCart) => {
       const existingItem = prevCart.find(cartItem => cartItem.id === item.id);
       if (existingItem) {
+
         return prevCart.map(cartItem =>
           cartItem.id === item.id ? { ...cartItem, quantity: cartItem.quantity + 1 } : cartItem
         );
       }
+
       return [...prevCart, { ...item, quantity: 1 }];
     });
   };
   const removeFromCart = (id: number) => {
     setCart((prevCart) => prevCart.filter(item => item.id !== id));
   };
+
   return (
     <CartContext.Provider value={{ cart, addToCart, removeFromCart }}>
       {children}
